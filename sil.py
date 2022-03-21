16e489930739c03e2852ef788e97cf0b9d3ab8ec
acuzumm <aliceemuzum@gmail.com>
2022-03-22 01:34:08 +0300

    initial commit

---
+++ sil.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def poly_func(x):
+    return
+
+dataset_x = np.array([1,2,3,4,5,6,7,8,9])
+dataset_y = np.array([1,2,3,2,1,0,4,7,5])
+
+plt.scatter(dataset_x,dataset_y)
+plt.show()
